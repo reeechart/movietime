17f3db8d229c33278730de894d31ca6f438ce189
reeechart <ferdinandusrichard@yahoo.co.id>
2018-06-08 23:02:31 +0700

    add search fragment

---
+++ app/src/main/java/com/reeechart/movietime/fragments/SearchFragment.kt
@@ -0,0 +1,25 @@
+package com.reeechart.movietime.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.reeechart.movietime.R
+
+/**
+ * Created by Richard on 08-Jun-18.
+ */
+class SearchFragment: Fragment() {
+    private val DEBUG_LOG: String = this.javaClass.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var rootView = inflater.inflate(R.layout.fragment_search, container, false)
+        return rootView
+    }
+}
