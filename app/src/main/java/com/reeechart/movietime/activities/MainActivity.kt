2053119329abc576da025293ba495a761463df74
reeechart <ferdinandusrichard@yahoo.co.id>
2018-06-08 23:03:20 +0700

    add fragment transactions to main activity

--- app/src/main/java/com/reeechart/movietime/activities/MainActivity.kt
+++ app/src/main/java/com/reeechart/movietime/activities/MainActivity.kt
@@ -1,26 +1,42 @@
 package com.reeechart.movietime.activities
 
-import android.graphics.Color
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
 import com.reeechart.movietime.R
+import com.reeechart.movietime.fragments.PlayingFragment
+import com.reeechart.movietime.fragments.SearchFragment
+import com.reeechart.movietime.fragments.UpcomingFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
         when (item.itemId) {
             R.id.navigation_playing -> {
-                message.setText(R.string.title_playing)
+                val playingFragment = PlayingFragment()
+
+                transaction.replace(R.id.fragmentContainer, playingFragment)
+                transaction.commit()
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_upcoming -> {
-                message.setText(R.string.title_upcoming)
+                val upcomingFragment = UpcomingFragment()
+
+                transaction.replace(R.id.fragmentContainer, upcomingFragment)
+                transaction.commit()
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_search -> {
-                message.setText(R.string.title_search)
+                val searchFragment = SearchFragment()
+
+                transaction.replace(R.id.fragmentContainer, searchFragment)
+                transaction.commit()
+
                 return@OnNavigationItemSelectedListener true
             }
         }
